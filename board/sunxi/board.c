lymzzyh/u-boot

commit 50ddbf1199a07badb7ef88a0b91889408320d121

sunxi: Register usb_ether
Call the function to register the usb_ether gadget in the board.

Reviewed-by: Łukasz Majewski <lukma@denx.de>
Reviewed-by: Simon Glass <sjg@chromium.org>
Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

board/sunxi/board.c
@@ -737,6 +737,8 @@ int misc_init_r(void)
 		return ret;
 #endif
 
+	usb_ether_init();
+
 	return 0;
 }
 

commit f031f1c79b5c08adcba32f08d36202443b622426

sunxi: only init USB Ethernet gadget when it's enabled
If the USB Ethernet gadget is not yet enabled, the call of
usb_ether_init in board/sunxi/board.c will lead to undefined reference
error when building.

Fix this problem.

Fixes: 50ddbf1199a0 ("sunxi: Register usb_ether")
Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

board/sunxi/board.c
@@ -733,7 +733,9 @@ int misc_init_r(void)
 		return ret;
 #endif
 
+#ifdef CONFIG_USB_ETHER
 	usb_ether_init();
+#endif
 
 	return 0;
 }